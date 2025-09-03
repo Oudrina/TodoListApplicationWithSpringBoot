066c4dddf5cfb04e8bd89eb532ca6b7576aa9b20
Oudrina Ceci-Foriwaa <oudrinaceciforiwaa.com>
2025-09-03 14:08:17 +0000

    First create the springboot project

---
+++ src/main/java/com/Todo/App/Todo_List/Application/with/springboot/TodoListApplicationWithSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.Todo.App.Todo_List.Application.with.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoListApplicationWithSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoListApplicationWithSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Todo/App/Todo_List/Application/with/springboot/TodoListApplicationWithSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Todo.App.Todo_List.Application.with.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoListApplicationWithSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
